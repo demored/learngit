217d2318850843cae575c95dfc9edbcbfe5850ad
pychecker <3091553181@qq.com>
2019-07-30 00:23:53 +0800

    py codes

--- def.py
+++ def.py
@@ -16,6 +16,7 @@ def sum_and_avg(list):
             sum += e
     return sum, sum / count
 my_list = [20, 15, 2.8, 'a', 35, 5.9, -1.8]
+
 # 获取sum_and_avg函数返回的多个值，多个返回值被封装成元组
 tp1,tp2= sum_and_avg(my_list)
 print(tp1)
